ee18910670140884ac4dd86f889618dedebb914d
wuyao20 <huyashangding@163.com>
2020-10-27 14:14:26 +0800

    assets manage

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import _ from 'lodash';
+import './style.css';
+import bg from './bg.jpg';
+import Data from './data/data.xml';
+import Notes from './data/data.csv';
 
 function component() {
   const element = document.createElement('div');
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = bg;
+  element.appendChild(myIcon);
+
+  console.log(Data);
+  console.log(Notes);
+
   return element;
 }
 
